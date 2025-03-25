629c285c18fe82b88d119fdec14a40fad561a0fc
hongsam14 <ghdtjdgus14@gmail.com>
2025-03-25 16:30:24 +0900

    Dev: add filter error to error package

---
+++ error/filterError.go
@@ -0,0 +1,19 @@
+package error
+
+import "fmt"
+
+type PolvoErrFilter int
+
+const (
+	ErrDetectionField PolvoErrFilter = iota
+)
+
+type PolvoFilterError struct {
+	Code   PolvoErrFilter
+	Origin error
+	Msg    string
+}
+
+func (e PolvoFilterError) Error() string {
+	return fmt.Sprintf("Polvo Filter Error: %s\n\t: %s", e.Msg, e.Origin.Error())
+}
